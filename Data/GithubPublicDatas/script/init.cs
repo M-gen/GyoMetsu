f310fd791cdbba9ffcad19d3a511880bb5415fb0
M-gen <operationKaratake@gmail.com>
2020-08-13 02:14:02 +0900

    起動時に読み込むスクリプトの追加

---
+++ Data/GithubPublicDatas/script/init.cs
@@ -0,0 +1,15 @@
+Title = "魚滅の槍";
+
+StartTitleScene();
+//StartTestScene();
+
+//StartBattleScene(@"data/script/battle_01.cs");
+//StartBattleScene(@"data/script/battle_02.cs");
+//StartBattleScene(@"data/script/battle_03.cs");
+
+//StartTalkScene(@"data/script/talk_script_01.cs");
+//StartTalkScene(@"data/script/talk_script_02.cs");
+//StartTalkScene(@"data/script/talk_script_03.cs");
+//StartTalkScene(@"data/script/talk_script_04.cs");
+//StartTalkScene(@"data/script/talk_script_05.cs");
+//StartTalkScene(@"data/script/talk_script_06.cs");
