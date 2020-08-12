bc50dc8800c7c0d187dc934c992d3213f2773617
M-gen <operationKaratake@gmail.com>
2020-08-12 22:42:52 +0900

    不要なファイル・ビルドイベントの削除と、素材適応スクリプトの更新

--- Data/GithubPublicDatas/atach.py
+++ Data/GithubPublicDatas/atach.py
@@ -1,25 +1,34 @@
 import glob, shutil, os
 
+root_relative_directory_path = "Project/GyoMetsu/Data/GithubPublicDatas"
+
 r = os.getcwd();
-root_directory = r[0:len(r)-len("Project/GyoMetsu/Data/PersonalDatas")]
-root_directory   = "G:/user/data_create/work_Activity/Fantasies&Foolers/"
-target_directory = root_directory + "Project/GyoMetsu/GyoMetsu/bin/Debug/data"
-src_directory    = root_directory + "Project/GyoMetsu/Data/GithubPublicDatas"
+root_directory = r[0:len(r)-len(root_relative_directory_path)]
 
-# ひとまず、既存のデータを削除する
-files = glob.glob(target_directory+'/*')
-for i in files:
-    shutil.rmtree(i)
+print(root_directory)
 
-# あとはコピーするだけ
-files = glob.glob(src_directory+'/*')
-for i in files:
-    
-    print("---")
-    print(i)
-    name = i[i.rfind('\\')+1:]
-    print(name)
-    if os.path.isdir(i):
-        shutil.copytree( i, target_directory+"/"+name )
-    else:
-        pass
+def CopyFiles( target_directory, src_directory ):
+    print("")
+    print("---CopyFiles--- {0} {1}".format(target_directory, src_directory))
+
+    # ひとまず、既存のデータを削除する
+    files = glob.glob(target_directory+'/*')
+    for i in files:
+        name = i[i.rfind('\\')+1:]
+        print("remove : {0}".format(name))
+        shutil.rmtree(i)
+
+    # あとはコピーするだけ
+    files = glob.glob(src_directory+'/*')
+    for i in files:
+        
+        name = i[i.rfind('\\')+1:]
+        if os.path.isdir(i):
+            shutil.copytree( i, target_directory+"/"+name )
+            #print(i)
+            print("copy : {0}".format(name))
+        else:
+            pass
+
+CopyFiles( root_directory + "Project/GyoMetsu/GyoMetsu/bin/Debug/data", root_directory + root_relative_directory_path)
+CopyFiles( root_directory + "Project/GyoMetsu/GyoMetsu/bin/Release/data", root_directory + root_relative_directory_path)
